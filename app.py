81a08b7b026abc248a8c77b45f6d1c1ddd51c73c
karardataflow <karardataflow@gmail.com>
2024-12-23 19:37:11 +0300

    a

--- app.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PySide6.QtWidgets import QApplication
-from controller import Controller
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)  # قم بإنشاء كائن واحد فقط
-    controller = Controller()
-    controller.view.show()
-    sys.exit(app.exec())
